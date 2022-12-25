cc58c17f88c3a01e37054e596a02c462463af86f
Mohamed Salah <mohamed_salah@rayacx.com>
2022-12-25 11:02:58 +0200

    Add project files.

---
+++ RDLCReportByAsp.net/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RDLCReportByAsp.net.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var data = GetActorInfo();
+            this.HttpContext.Session["Data"] = data.Tables[0];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public void GetActorReport()
+        {
+            var data = GetActorInfo();
+            this.HttpContext.Session["Data"] = data.Tables[0];
+        }
+
+        private DataSet GetActorInfo()
+        {
+            var Constr = @"Data Source=DESKTOP-T64S8Q3;Database=Movies;Trusted_Connection=True;";
+            var dataset = new DataSet();
+            var sql = "EXEC SPGetActorInfo";
+            var con = new SqlConnection(Constr);
+
+            var cmd = new SqlCommand(sql, con);
+            var adpt = new SqlDataAdapter(cmd);
+            adpt.Fill(dataset);
+            return dataset;
+
+
+
+
+        }
+    }
+}
---
+++ RDLCReportByAsp.net/Reports/ActorReport.aspx.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+
+
+namespace RDLCReportByAsp.net.Reports
+{
+    public partial class ActorReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadReport();
+            }
+        }
+
+        private void LoadReport()
+        {
+            Page.Title = "Moo Report";
+            var dt = new DataTable();
+            dt = (DataTable)HttpContext.Current.Session["Data"];
+            if (dt.Rows.Count > 0)
+            {
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportViewer1.LocalReport.DataSources.Add(new ReportDataSource("dsActor", dt));
+                ReportViewer1.LocalReport.ReportPath = Server.MapPath("~//Reports//report//ActorReport.rdlc");
+                ReportViewer1.DataBind();
+                ReportViewer1.LocalReport.Refresh();    
+
+
+            }
+        }
+    }
+}
