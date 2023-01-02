3fbeecb8e72ef05536b2259f7fd533909aadd1cd
Mohamed Salah <mohamed_salah@rayacx.com>
2023-01-02 13:53:43 +0200

    Work with Data Base And Auth With FireBase

---
+++ RDLCReportByAsp.net/Scripts/FirebaseConfig.js
@@ -0,0 +1,12 @@
+﻿
+const config = {
+    apiKey: "AIzaSyB4ORpeBRAYmN4uMUH9m2-wNXAyQcoDsc4",
+    authDomain: "rdlc-report.firebaseapp.com",
+    projectId: "rdlc-report",
+    storageBucket: "rdlc-report.appspot.com",
+    messagingSenderId: "501534950048",
+    appId: "1:501534950048:web:ec442890e1a4088049ffc5",
+    measurementId: "G-4Z8RHWE4DQ"
+};
+
+firebase.initializeApp(config);
